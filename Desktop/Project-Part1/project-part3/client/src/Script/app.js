7e9912c2d0600a37f849f18f622395d248d68cc8
Shifa-Faisal <shifa.faisal@ontariotechu.net>
2025-12-03 21:16:00 -0500

    basic angular client side added

---
+++ Desktop/Project-Part1/project-part3/client/src/Script/app.js
@@ -0,0 +1,8 @@
+// IIFI
+// immidiately invoked function expression
+(function(){
+    function start(){
+        console.log("Client side app working");
+    }
+    window.addEventListener("load",start);
+})();
